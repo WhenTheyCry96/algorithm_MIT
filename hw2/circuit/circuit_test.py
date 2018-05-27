8821c839b71f49c9b035a9796f43286465e977d1
pajoheji099 <pajoheji0909@gmail.com>
2018-05-28 00:07:42 +0200

    minor fix

--- hw2/circuit/circuit_test.py
+++ hw2/circuit/circuit_test.py
@@ -4,7 +4,7 @@ import unittest
 import sys
 import glob
 import re
-from circuit import *
+from circuit_1 import *
 
 class CircuitTest(unittest.TestCase):
     def setUp(self):
@@ -12,7 +12,7 @@ class CircuitTest(unittest.TestCase):
         # Obtains a list of cross-platform test file paths.
         self._in_files = glob.glob(os.path.join(dir, 'tests', '*.in'))
         self._in_files.sort()
-    
+
     def _cmp_files(self, file, lines):
         for line in lines:
             if line != file.readline().strip():
@@ -28,16 +28,15 @@ class CircuitTest(unittest.TestCase):
                 sim = Simulation.from_file(in_file)
                 sim.run()
                 out_lines = sim.outputs_to_line_list()
-               
+
                 gold_filename = re.sub('\.in$', '.gold', in_filename)
                 with open(gold_filename) as gold_file:
                     same = self._cmp_files(gold_file, out_lines)
                     if same:
                         print 'OK'
-                    else: 
+                    else:
                         print 'Failed'
                     self.assertTrue(same)
-    
+
 if __name__ == '__main__':
     unittest.main()
-
